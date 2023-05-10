8d186359f293d54bbef2b2996600662e0ee6822d
Christian Baronia <christianbaronia3@gmail.com>
2023-05-10 09:50:27 -0700

    Add image loading, allowing images to be imported in JS file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import "./style.css";
+import Sky from "./sky.png";
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = "Hello, World!";
   element.classList.add("test");
+
+  // Add the image to our existing div.
+  const theSky = new Image();
+  theSky.src = Sky;
+
+  element.appendChild(theSky);
 
   return element;
 }

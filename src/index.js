4dfed9e70306deefde2a758cd91eeec678581a6e
cbaronia <86375882+cbaronia@users.noreply.github.com>
2025-03-26 15:16:14 -0700

    Remove setup content

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-import "./styles.css";
-import {greeting} from "./greeting.js";
-import odinImage from "./odin.svg";
 
-const image = document.createElement("img");
-image.src = odinImage;
-   
-document.body.appendChild(image);
-
-console.log(greeting);

46fcf4d19a7ba86d94fb10f50fd18175f7318468
cbaronia <86375882+cbaronia@users.noreply.github.com>
2025-04-02 17:58:47 -0700

    Add clear page function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import homePage from "./home.js";
 import menuPage from "./menu.js";
 import contactPage from "./contact.js";
 
-// homePage.createHomePage();
-// menuPage.createMenuPage();
-contactPage.createContactPage();
+const viewController = (function () {
+
+    const _clearPage = () => {
+        // Get the 'content' container
+        const contentContainer = document.getElementById("content");
+
+        // Remove all children elements
+        while(contentContainer.firstChild){
+            contentContainer.removeChild(contentContainer.lastChild);
+        }
+    };
+
+    return{};
+
+})();
+
+// Load homePage by default
+homePage.createHomePage();
